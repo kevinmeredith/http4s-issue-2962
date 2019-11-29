c704bcd49ddb83cec0be6f2f9aa36400b079b9f1
kmeredith <kevin.meredith@banno.com>
2019-11-29 17:50:25 -0500

    Add files for attempting to reproduce 2962.

---
+++ build.sbt
@@ -0,0 +1,7 @@
+scalaVersion := "2.12.10"
+
+val http4sVersion = "0.20.13"
+
+libraryDependencies += "org.http4s" %% "http4s-blaze-client" % http4sVersion
+libraryDependencies += "org.http4s" %% "http4s-core" % http4sVersion
+libraryDependencies += "org.http4s" %% "http4s-client" % http4sVersion
---
+++ src/main/scala/net/Test.scala
@@ -0,0 +1,40 @@
+package net
+
+import java.util.concurrent.atomic.AtomicInteger
+import cats.effect.{IO, Timer}
+import fs2.Stream
+import org.http4s.Request
+import org.http4s._
+import org.http4s.client.blaze.BlazeClientBuilder
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    val int                       = new AtomicInteger(0)
+    implicit val CS               = IO.contextShift(ExecutionContext.global)
+    implicit val timer: Timer[IO] = IO.timer(ExecutionContext.global)
+    val timeout                   = 1 seconds
+
+    val program = for {
+      client <- BlazeClientBuilder[IO](scala.concurrent.ExecutionContext.global)
+        .withRequestTimeout(timeout)
+        .withResponseHeaderTimeout(timeout)
+        .allocated
+      c      = client._1
+      status = c.status(Request[IO](uri = uri"""http://httpbin.org/status/500""")).attempt
+      _ <- Stream(Stream.eval(status)).repeat
+        .covary[IO]
+        .parJoin(100)
+        .take(1000)
+        .observe(x => x.flatMap(y => Stream.eval(IO(println(">>> " + int.incrementAndGet + " " + y)))))
+        .compile
+        .drain
+      s <- c.status(Request[IO](uri = uri"""http://httpbin.org/status/500""")).attempt
+      _ <- IO(println("STATUS = " + s.right.get))
+    } yield ()
+
+    program.unsafeRunSync()
+  }
+}
